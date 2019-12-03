3a6e2df89ea59c141703c770442baeac99e9048c
p308945 <895175589@qq.com>
2019-12-03 14:27:40 +0800

    Update process_info.cpp

--- common/process_info.cpp
+++ common/process_info.cpp
@@ -159,7 +159,7 @@ bool GetCpuUseInfo(int &cpuPercent)
 	if (0 == deltTotal)
 	{
 		cpuPercent = 0;
-		LOG_ERROR("GetCpuUseInfo fail deltTotal == 0 info :");
+		LOG_ERROR("GetCpuUseInfo fail deltTotal == 0 %d info :", deltProc);
 		procCpuInfo.LogError("procCpuInfo");
 		lastProcCpuInfo.LogError("lastProcCpuInfo");
 		totalCpuInfo.LogError("totalCpuInfo");
@@ -167,8 +167,8 @@ bool GetCpuUseInfo(int &cpuPercent)
 	}
 	else
 	{
-		cpuPercent = deltProc / deltTotal;
-		LOG_DEBUG("GetCpuUseInfo succ %d info:", cpuPercent);
+		cpuPercent = 100 * deltProc / deltTotal;
+		LOG_DEBUG("GetCpuUseInfo succ %d %d %d info:", cpuPercent, deltProc, deltTotal);
 		procCpuInfo.LogDebug("procCpuInfo");
 		lastProcCpuInfo.LogDebug("lastProcCpuInfo");
 		totalCpuInfo.LogDebug("totalCpuInfo");
